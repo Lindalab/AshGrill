1588ae8e2b4ef50eaff1859d091382d4518737ff
Linda Arthur <78045124+Lindalab@users.noreply.github.com>
2022-11-23 15:20:46 +0000

    Add files via upload

--- Functions/display_service.php
+++ Functions/display_service.php
@@ -4,32 +4,37 @@ require_once("../Controllers/service_controller.php");
 function showServicesUsers_fnc()
 {
     $mydata = getAllService_ctr();
+    echo "<table class='table'>
+    <thead class='table-dark'>
+      <tr>
+      <th scope='col'>Serivice Name Day</th>
+        <th scope='col'>Service Price</th>
+       
+        
+      </tr>
+    </thead>
+    <tbody>";
 
     foreach ($mydata as $brand) {
         $serviceId = $brand['s_id'];
         $servicename = $brand['service_name'];
         $serviceimage= $brand['service_image'];
         $serviceprice = $brand['price'];
-        $cateName = $brand['cat_name '];
-        showServicesRow_fnc($serviceId , $servicename, $serviceimage,$serviceprice,$cateName);
+       // $cateName = $brand['cat_name '];
+        showServicesRow_fnc($serviceId , $servicename, $serviceimage,$serviceprice);
     }
     
 }
 
-function showServicesRow_fnc($serviceId , $servicename, $serviceimage,$serviceprice,$cateName)
+function showServicesRow_fnc($serviceId , $servicename, $serviceimage,$serviceprice)
 {
-    echo "
-    <div class='card' style='width: 18rem;'>
-  <img class='card-img-top' src='../$serviceimage' alt='Card image cap'>
-  <div class='card-body'>
-    <h5 class='card-title'>$servicename</h5>
-    <p class='card-text'>Category :$cateName</p>
-  </div>
-  <ul class='list-group list-group-flush'>
-    <li class='list-group-item'$serviceprice</li>
-  </ul>
-</div>
-    ";
+  echo "
+  <tr>
+  <td>$servicename</td>
+    <td>$serviceprice</td>
+    
+  </tr>
+  ";
 }
 
 ?>
